411f88d0bc067b4aa669f45189af753b656fd14b
Brandon Jenkins <brxndonbrxhmusic@gmail.com>
2024-09-27 11:18:25 +0700

    Image Performance Enhancement

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,7 @@
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const currentPage = window.location.pathname.split('/').pop() || 'index.html';
     const navLinks = document.querySelectorAll('#nav-link');
